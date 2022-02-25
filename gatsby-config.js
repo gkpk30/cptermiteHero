7ffc77bfd0784bc865e6642e0917a8b158473248
gkpk30 <guykoomer@hotmail.com>
2022-02-24 19:45:38 -0800

    added gatsby-plugin-netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,6 @@
 module.exports = {
   plugins: [
+    'gatsby-plugin-netlify',
     'gatsby-plugin-top-layout',
     'gatsby-plugin-react-helmet',
     // If you want to use styled components you should add the plugin here.
