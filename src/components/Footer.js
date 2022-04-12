57cad35f7be19771e58d835541fcd9694e8d81af
gkpk30 <guykoomer@hotmail.com>
2022-04-12 15:03:07 -0700

    flexed footer lists

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -28,7 +28,8 @@ export default function Footer() {
         <Box sx={{backgroundColor:"primary.main", color: 'text.secondary', minHeight: '5vh'}}  >
             <Container maxWidth="lg" sx={{paddingTop: 6}}>
                 <Box display="flex" justifyContent='space-between'   sx={{flexDirection:{xs: 'column ', md: 'row'}, gap: 3}}>
-                    <Box flex='5' sx={{paddingRight: 5}} >
+                    
+                    <Box flex='4' sx={{paddingRight: 5}} >
                         <Typography color='text.main' >Captain Termite Control</Typography>
                         <Typography mt={2} > Providing the highest level of service at affordable prices
                             Whether it's your home or business we can protect your investment by ridding your property of unwanted termites.
@@ -36,66 +37,70 @@ export default function Footer() {
 
                         <Social isFooter = 'row' color="third"/>
                     </Box>
+                    <Box flex= '6' display="flex"  flexWrap="wrap" 
+                        sx={{ gap: 4}}
+                       
+                    >
+                        <Box flex='2'>
+                        <Link to='/treatments'><Typography color='text.main'  >Treatments</Typography></Link>
+                            <ul style={{listStyle: 'none', paddingLeft: '0'}}>
+                                {treatmentLinks.map((link) => {
+                                    return(
+                                        <li key={link.frontmatter.title}>
+                                            <Link to={`/treatments/${link.frontmatter.slug}`} sx={{color: 'text.secondary', fontWeight: 'medium', fontSize:'.72rem'}}>
+                                                {link.frontmatter.title}
+                                            </Link>
+                                        </li>
+                                        )  
+                                })}
+                            </ul>
                     
-                    <Box flex='2'>
-                    <Link to='/treatments'><Typography color='text.main'  >Treatments</Typography></Link>
+                        </Box>
+                        <Box flex='2'>
+                        <Link to="/termiteinfo" ><Typography color='text.main' > Termite Info</Typography></Link>
+                        
                         <ul style={{listStyle: 'none', paddingLeft: '0'}}>
-                            {treatmentLinks.map((link) => {
-                                return(
-                                    <li key={link.frontmatter.title}>
-                                        <Link to={`/treatments/${link.frontmatter.slug}`} sx={{color: 'text.secondary', fontWeight: 'medium', fontSize:'.72rem'}}>
-                                            {link.frontmatter.title}
+                            <li>
+                                <Link to="/termiteinfo" sx={{color: 'text.secondary', fontWeight: 'medium', fontSize:'.72rem'}}>
+                                
+                                            Drywood
+                                    
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/termiteinfo" sx={{color: 'text.secondary', fontWeight: 'medium', fontSize:'.72rem'}}>
+                                
+                                            Subterranean
+                                    
+                                </Link>
+                            </li>
+                            
+                        </ul>
+                        </Box>
+                        <Box flex='2'>
+                            <Link to="/contact"><Typography color='text.main'  >Contact Us</Typography></Link>
+                                <ul style={{listStyle: 'none', paddingLeft: '0'}}>
+                                    <li style={{marginBottom: '4px'}}>
+                                        <Link  to="/contact" sx={{color: 'text.secondary', fontWeight: 'medium', fontSize:'.72rem'}} >
+                                            
+                                                Contact Our Team
+                                            
                                         </Link>
                                     </li>
-                                    )  
-                            })}
-                        </ul>
-                   
-                    </Box>
-                    <Box flex='2'>
-                    <Link to="/termiteinfo" ><Typography color='text.main' > Termite Info</Typography></Link>
-                       
-                       <ul style={{listStyle: 'none', paddingLeft: '0'}}>
-                           <li>
-                            <Link to="/termiteinfo" sx={{color: 'text.secondary', fontWeight: 'medium', fontSize:'.72rem'}}>
-                            
-                                        Drywood
-                                
-                            </Link>
-                           </li>
-                           <li>
-                            <Link to="/termiteinfo" sx={{color: 'text.secondary', fontWeight: 'medium', fontSize:'.72rem'}}>
-                            
-                                        Subterranean
-                                
-                            </Link>
-                           </li>
-                           
-                       </ul>
-                    </Box>
-                    <Box flex='2'>
-                        <Link to="/contact"><Typography color='text.main'  >Contact Us</Typography></Link>
-                            <ul style={{listStyle: 'none', paddingLeft: '0'}}>
-                                <li style={{marginBottom: '4px'}}>
-                                    <Link  to="/contact" sx={{color: 'text.secondary', fontWeight: 'medium', fontSize:'.72rem'}} >
+                                    <li>
+                                        <Typography gutterBottom sx={{color: 'text.secondary', fontWeight: 'medium', fontSize:'.72rem'}}>
+                                            818-822-6782
+                                        </Typography>
                                         
-                                            Contact Our Team
-                                        
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Typography gutterBottom sx={{color: 'text.secondary', fontWeight: 'medium', fontSize:'.72rem'}}>
-                                        818-822-6782
-                                    </Typography>
-                                    
-                                </li>
-                                <li>
-                                    <Typography gutterBottom sx={{color: 'text.secondary', fontWeight: 'medium', fontSize:'.72rem'}}>
-                                        CaptainTermiteControlInc@gmail.com
-                                    </Typography>
-                                </li>
-                            </ul>
-                       
+                                    </li>
+                                    <li>
+                                        <Typography gutterBottom sx={{color: 'text.secondary', fontWeight: 'medium', fontSize:'.72rem'}}>
+                                            CaptainTermiteControlInc@gmail.com
+                                        </Typography>
+                                    </li>
+                                </ul>
+                        
+                        </Box>
                     </Box>
                 </Box>
                 <Divider  sx={{borderColor: 'text.secondary', my:4}}/>
